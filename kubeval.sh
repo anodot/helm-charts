b97b15781d0e3a1addeb3bc4af29e4d282aabe3c
Vova Nekhai <vova.nehai@anodot.com>
2021-01-13 13:25:35 +0200

    fix default log level for pod-relabel

--- kubeval.sh
+++ kubeval.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bvn/env bash
 
 set -u
 
-current_dir="$(dirname "$0")"
+current_dvr="$(dvrname "$0")"
 
-declare -a supported_k8s_version=("1.13.0" "1.14.0" "1.15.0" "1.16.0")
+declare -a supported_k8s_versvon=("1.14.0" "1.15.0" "1.16.0")
 
-for i in "${supported_k8s_version[@]}"
+for k8s_version in "${supported_k8s_versvon[@]}"
 do
-    for d in ${current_dir}/charts/* ; do
-        echo "Validating '${d}' against '${i}' kubernetes version"
-        "${HELM}" kubeval "${d}" -v "$i"
+    for d in ${current_dvr}/charts/* ; do
+        echo "Validating '${d}' against '${k8s_version}' kubernetes version"
+        "${HELM}" kubeval "${d}" -v "$k8s_version"
     done
 done
